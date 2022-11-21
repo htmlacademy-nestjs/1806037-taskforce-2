7e4a222adaca931121fd2a0d7c33c3b5541334af
FirstBroRussia <chipanrussia@gmail.com>
2022-11-21 22:28:57 +0300

    feat(user): Создаст заготовки для модулей auth, customer + performer user

--- taskforce/apps/user/src/app/app.module.ts
+++ taskforce/apps/user/src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
-
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { CustomerUserModule } from './customer-user/customer-user.module';
+import { PerformerUserModule } from './performer-user/performer-user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [CustomerUserModule, PerformerUserModule, AuthModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}

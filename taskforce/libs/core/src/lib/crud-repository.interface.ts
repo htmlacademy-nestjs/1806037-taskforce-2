41dcb27420c3d2ec884bf8b2e789bf3552ea919d
FirstBroRussia <chipanrussia@gmail.com>
2022-11-21 22:59:26 +0300

    fix:name reps

--- taskforce/libs/core/src/lib/crud-repository.interface.ts
+++ taskforce/libs/core/src/lib/crud-repository.interface.ts
@@ -1,6 +1,6 @@
 // the abbreviation 'E' from the word 'Entity'
 
-export  interface CRUDInterface<E, I, R> {
+export  interface CRUDRepositoryInterface<E, I, R> {
   create(item: E): Promise<R | null>;
   findById(id: I): Promise<R>;
   update(id: I, item: E): Promise<R>;
